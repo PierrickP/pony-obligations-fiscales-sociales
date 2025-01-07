aaa06af104d2e17cf131aad2d1666c4c9c93c907
Pierrick <mulotmail@gmail.com>
2025-01-07 14:08:03 +0100

    Add error/question footer
    Signed-off-by: Pierrick <mulotmail@gmail.com>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -73,5 +73,17 @@
 		administrations fiscales et sociales françaises. En cas du moindre doute, rappochez-vous d'un comptable.
 	</div>
 
+	<div class="mx-auto text-center text-xs text-gray-500 md:w-1/2">
+		Vous remarquez une erreur ou avez une question ? Créez une issue sur <a
+			href="https://github.com/PierrickP/pony-obligations-fiscales-sociales"
+			target="_blank"
+			class="underline hover:no-underline">GitHub</a
+		>
+		ou rejoinger le serveur
+		<a href="https://discord.gg/6ZjyXMu9" target="_blank" class="underline hover:no-underline"
+			>Discord</a
+		>
+	</div>
+
 	<div class="mx-auto md:w-1/2"><Discord></Discord></div>
 </div>
